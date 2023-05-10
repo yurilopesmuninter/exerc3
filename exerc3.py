1e4fa773aeb5b4c6250032547f196b18fb74b58b
root <root@NOTE032973.grupouninter.local>
2023-05-10 15:57:56 -0300

    exerc3 finalizado com sucesso

--- exerc3.py
+++ exerc3.py
@@ -19,11 +19,11 @@ def dimensoesObjeto():
                 print('O volume do objeto é (em cm³): {}'.format(volume))
                 if 0 < volume < 1000:
                     valor = 10
-                elif 1000 < volume < 10000:
+                elif 1000 <= volume < 10000:
                     valor = 20
-                elif 10000 < volume < 30000:
+                elif 10000 <= volume < 30000:
                     valor = 30
-                elif 30000 < volume < 100000:
+                elif 30000 <= volume < 100000:
                     valor = 50
                 elif volume > 100000:
                     print('Não aceitamos objetos com dimensões tão grandes\nEntre com as dimensões desejadas novamente')
@@ -54,16 +54,36 @@ def pesoObjeto():
             print('Você digitou peso do objeto com valor não numérico\nPor favor entre com o peso desejado novamente')
             continue
 def rotaObjeto():
-    rotas = {
-        RS: {percurso: 'De Rio de Janeiro até São Paulo', multiplicador: 1},
-        SR: {percurso: 'De São Paulo até Rio de Janeiro', multiplicador: 1}
-    }
-    print('Selecione a rota:')
-    print(' BR - De Brasília para Rio de Janeiro')
-    print(' BS - De Brasília para São Paulo')
-    print(' RB - De Rio de Janeiro para Brasília')
+    rotas = [
+        {'sigla': 'RS', 'percurso': 'De Rio de Janeiro até São Paulo', 'multiplicador': 1},
+        {'sigla': 'SR', 'percurso': 'De São Paulo até Rio de Janeiro', 'multiplicador': 1},
+        {'sigla': 'BS', 'percurso': 'De Brasília até São Paulo', 'multiplicador': 1.2},
+        {'sigla': 'SB', 'percurso': 'De São Paulo até Brasília', 'multiplicador': 1.2},
+        {'sigla': 'BR', 'percurso': 'De Brasília até Rio de Janeiro', 'multiplicador': 1.5},
+        {'sigla': 'RB', 'percurso': 'De Rio de Janeiro até Brasília', 'multiplicador': 1.5},
+    ]
+    siglas = ['RS', 'SR', 'BS', 'SB', 'BR', 'RB']
+    while True:
+        print('Selecione a rota:')
+        for rota in rotas:
+            print(' {} - {}'.format(rota['sigla'], rota['percurso']))
+        escolhido = input('')
+        if escolhido.upper() not in siglas:
+            print('Você digitou uma rota que não existe\nPor favor entre com a rota desejada novamente')
+            continue
+        if escolhido.upper() == 'RS' or escolhido.upper() == 'SR':
+            mult = 1
+        elif escolhido.upper() == 'BS' or escolhido.upper() == 'SB':
+            mult = 1.2
+        elif escolhido.upper() == 'BR' or escolhido.upper() == 'RB':
+            mult = 1.5
+        else:
+            print('Você digitou uma rota que não existe\nPor favor entre com a rota desejada novamente')
+            continue
+        return mult
 print('Bem Vindo a Companhia de Logística Yuri Lopes Maria')
 dimensao = dimensoesObjeto()
 peso = pesoObjeto()
 rota = rotaObjeto()
-
+total = dimensao * peso * rota
+print('Total a pagar(R$): {} (dimensões: {} * peso: {} * rota: {})'.format(total, dimensao, peso, rota))
